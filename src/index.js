dac432ee4b2a36a6408a0232babab6f083a03a1c
yura-1990 <yurayur1990@gmail.com>
2024-11-21 15:41:10 +0500

    theme color

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
 import { initI18n } from './i18n';
 
 initI18n('en');
