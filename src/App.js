f64b40699ee76e1dd255c1cb71ac99f8de9ddd9d
Murod Atamirzayev <m.atamirzayev@uzinfocom.com>
2024-11-21 13:21:34 +0500

    web bot init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import useTelegram from './hooks/useTelegram';
 import { initI18n } from './i18n';
 import Header from "./components/header";
@@ -8,8 +7,7 @@ import Home from "./pages/home";
 
 const App = () => {
 
-    const { t } = useTranslation();
-    const { isReady, user, language } = useTelegram();  // Get the language from Telegram hook
+    const { isReady, language } = useTelegram();  // Get the language from Telegram hook
 
     const [loading, setLoading] = useState(true);
 
